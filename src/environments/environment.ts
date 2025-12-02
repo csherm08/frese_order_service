f1000f0e76e4e2732dec76d0c01e1c8384cbd97e
Carter Sherman <carterjsherman@gmail.com>
2025-12-02 15:20:45 -0500

    fix api endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   env: 'default',
   stripe: 'pk_live_51KasQqEZvpspKOfSzW7sdVtBJmH1pVuJ7MkqdkFvwMqH1FG2RkSdpI5qDqEzsxeNgUOwODddzocbKqlRu90DAnMA00Y537FNq1',
-  baseUrl: 'https://frese-bakery-backend-app-eqjx4stexa-uc.a.run.app/api'
+  baseUrl: 'https://frese-bakery-backend-app-504689514656.us-east1.run.app/api'
 };
 
 /*
